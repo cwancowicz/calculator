d8b3c55390613410977136d731f56cfa6b188e63
Chris Wancowicz <cwancowicz@gmail.com>
2018-03-17 08:18:42 -0400

    got somewhere with the styling and classes

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -1,35 +1,87 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var equation = [];
+    addClasses();
     addButtonListeners(equation);
 });
 
 function addButtonListeners(eq) {
-    $("#1").click(function() {buildNumber(1, eq)});
-    $("#2").click(function() {buildNumber(2, eq)});
-    $("#3").click(function() {buildNumber(3, eq)});
-    $("#4").click(function() {buildNumber(4, eq)});
-    $("#5").click(function() {buildNumber(5, eq)});
-    $("#6").click(function() {buildNumber(6, eq)});
-    $("#7").click(function() {buildNumber(7, eq)});
-    $("#8").click(function() {buildNumber(8, eq)});
-    $("#9").click(function() {buildNumber(9, eq)});
-    $("#0").click(function() {buildNumber(0, eq)});
-    $("#point").click(function() {buildNumber(".", eq)});
 
-    $("#plus").click(function() {buildOperand("+", eq)});
-    $("#minus").click(function() {buildOperand("-", eq)});
-    $("#multiply").click(function() {buildOperand("*", eq)});
+    $("#1").click(function () {
+        buildNumber(1, eq)
+    });
+    $("#2").click(function () {
+        buildNumber(2, eq)
+    });
+    $("#3").click(function () {
+        buildNumber(3, eq)
+    });
+    $("#4").click(function () {
+        buildNumber(4, eq)
+    });
+    $("#5").click(function () {
+        buildNumber(5, eq)
+    });
+    $("#6").click(function () {
+        buildNumber(6, eq)
+    });
+    $("#7").click(function () {
+        buildNumber(7, eq)
+    });
+    $("#8").click(function () {
+        buildNumber(8, eq)
+    });
+    $("#9").click(function () {
+        buildNumber(9, eq)
+    });
+    $("#0").click(function () {
+        buildNumber(0, eq)
+    });
+    $("#point").click(function () {
+        buildNumber(".", eq)
+    });
 
-    $("#divide").click(function() {buildOperand("/", eq)});
-    $("#clear").click(function() {buildClearButton(eq)});
+    $("#plus").click(function () {
+        buildOperand("+", eq)
+    });
+    $("#minus").click(function () {
+        buildOperand("-", eq)
+    });
+    $("#multiply").click(function () {
+        buildOperand("*", eq)
+    });
 
-    $("#equalsButton").click(function() {evaluate(eq)});
+    $("#divide").click(function () {
+        buildOperand("/", eq)
+    });
+    $("#clear").click(function () {
+        buildClearButton(eq)
+    });
+
+    $("#equalsButton").click(function () {
+        evaluate(eq)
+    });
+}
+
+function addClasses() {
+    var buttons = ["#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9", "#0", "#point", "#clear", "#plus", "#minus", "#multiply", "#divide"];
+    addClassToElement(buttons, "btn-block");
+
+    var buttonDivs = ["#div1", "#div2", "#div3", "#div4", "#div5",
+        "#div6", "#div7", "#div8", "#div9",
+        "#div0", "#divP", "#divC", "#divAdd", "#divSub", "#divMult", "#divDivide"];
+    addClassToElement(buttonDivs, "col-sm-1");
+}
+
+function addClassToElement(elements, className) {
+    elements.forEach(function (value) {
+        $(value).addClass(className);
+    });
 }
 
 function buildNumber(num, eq) {
     var existingNumber = getLastNumber(eq, num);
     if (num != "." || !existingNumber.includes(".")) {
-        eq[eq.length-1] = existingNumber + num;
+        eq[eq.length - 1] = existingNumber + num;
     }
 }
 
@@ -38,7 +90,7 @@ function buildOperand(op, eq) {
         eq.push(op);
     } else {
         // overwrite current op
-        eq[eq.length-1] = op;
+        eq[eq.length - 1] = op;
     }
 }
 
@@ -55,11 +107,11 @@ function getLastNumber(equation, curNum) {
     if (equation.length == 0 || lastOperandWasNotANumber(equation)) {
         equation.push("");
     }
-    return equation[equation.length-1];
+    return equation[equation.length - 1];
 }
 
 function lastOperandWasNotANumber(eq) {
-    return operandNotANumber(eq[eq.length-1]);
+    return operandNotANumber(eq[eq.length - 1]);
 }
 
 function lastOperandWasANumber(eq) {
@@ -67,7 +119,7 @@ function lastOperandWasANumber(eq) {
 }
 
 function operandNotANumber(num) {
-    switch(num) {
+    switch (num) {
         case "+":
         case "-":
         case "*":
